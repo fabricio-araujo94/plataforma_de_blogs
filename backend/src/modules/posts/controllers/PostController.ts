8eb5e718cec1d9cbc054a6105cb8c6b0d4b43917
Fabricio <fabricio.araujo61@aluno.ifce.edu.br>
2026-03-11 18:53:38 -0300

    feat: creating PostController for validating posts and making requests

---
+++ backend/src/modules/posts/controllers/PostController.ts
@@ -0,0 +1,59 @@
+import { Request, Response } from "express";
+import { PostService } from "../services/PostService";
+import { createPostSchema, updatePostSchema } from "../dtos/PostDTO";
+
+export class PostController {
+  static async create(req: Request, res: Response) {
+    try {
+      if (!req.user) {
+        return res.status(401).json({ error: "Unauthenticated user." });
+      }
+
+      const data = createPostSchema.parse(req.body);
+
+      const post = await PostService.create(req.user.id, data);
+
+      res.status(201).json(post);
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        res.status(400).json({ error: err.message });
+      } else {
+        res.status(500).json({ error: "Internal server error." });
+      }
+    }
+  }
+
+  static async update(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+
+      if (!id || Array.isArray(id)) {
+        return res.status(400).json({ error: "Invalid post id." });
+      }
+
+      if (!req.user) {
+        return res.status(401).json({ error: "Unauthenticated user." });
+      }
+
+      const data = updatePostSchema.parse(req.body);
+
+      const post = await PostService.update(
+        id,
+        req.user.id,
+        req.user.role,
+        data,
+      );
+
+      res.status(200).json(post);
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        if (err.message.includes("Access denied.")) {
+          return res.status(403).json({ error: err.message });
+        }
+        res.status(400).json({ error: err.message });
+      } else {
+        res.status(500).json({ error: "Internal server error." });
+      }
+    }
+  }
+}
