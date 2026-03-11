6503adc72f7ce100906acf169c714ae7bc04fc69
Fabricio <fabricio.araujo61@aluno.ifce.edu.br>
2026-03-11 18:59:48 -0300

    refactor: addition of post routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,7 +2,11 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import helmet from "helmet";
+
 import { AuthController } from "./modules/users/controllers/AuthController";
+import { PostController } from "./modules/posts/controllers/PostController";
+
+import { authMiddleware } from "./shared/middlewares/authMiddleware";
 
 const app = express();
 
@@ -19,5 +23,8 @@ app.use(cookieParser());
 
 app.post("/api/auth/register", AuthController.register);
 app.post("/api/auth/login", AuthController.login);
+
+app.post("/api/posts", authMiddleware, PostController.create);
+app.post("/api/posts/:id", authMiddleware, PostController.update);
 
 export { app };
